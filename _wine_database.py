f36fcbf143c601296046a3127c2d8d2b7ec94a52
Luke Russell Wurl <lwurl@student04.cse.nd.edu>
2018-11-05 18:17:11 -0500

    Put data into wines, users, and reviews

--- _wine_database.py
+++ _wine_database.py
@@ -6,6 +6,7 @@ class _wine_database:
     def __init__(self):
         self.wines = {}
         self.users = {}
+        self.reviews = {}
 
     def load_all(self, movie_file):
         df = pd.read_csv(movie_file)
@@ -13,6 +14,21 @@ class _wine_database:
         df = df.assign(variety_id=(df['variety']).astype('category').cat.codes)
         df = df.assign(bottle_id=(df['title']).astype('category').cat.codes)
         df = df.drop(['region_1', 'region_2'], axis=1)
+        self.reviews = df
+        for index, row in df.iterrows():
+            if row['taster_id'] not in self.users:
+                self.users[row['taster_id']] = {'name': row['taster_name'], 'twitter': row['taster_twitter_handle']}
+            if row['bottle_id'] not in self.wines:
+                self.wines[row['bottle_id']] = {
+                    'title': row['title'],
+                    'variety': row['variety'],
+                    'province': row['province'],
+                    'price': row['price'],
+                    'winery': row['winery'],
+                    'designation': row['designation'],
+                    'country': row['country']
+                }
+
 
 if __name__ == '__main__':
     wines = _wine_database()
